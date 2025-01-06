d464d9131709a9af5c4fcb2122684d64b0b855b6
Enkhtuvshin Narmandakh <37796969+Enkhtuvshin0513@users.noreply.github.com>
2025-01-06 18:16:21 +0800

    suuri

--- index.js
+++ index.js
@@ -1,28 +1,70 @@
 let balance = 10000;
-const result = [];
+let gameResult = [];
 
 const gameChooser = () => {
-  const game = parseInt(prompt("1 2 3 "));
+  const userChoise = parseInt(
+    prompt("Yamar togloom togloh we ? 1=haich chuluu 2=coin toss 3=dice")
+  );
 
-  switch (game) {
+  let chosenGame;
+
+  switch (userChoise) {
     case 1:
-      return haichChuluu();
+      chosenGame = haichChuluu;
+      break;
     case 2:
-      return coinToss();
+      chosenGame = coinToss;
+      break;
     case 3:
-      return dice();
+      chosenGame = dice;
+      break;
+
+    default:
+      chosenGame = null;
   }
+
+  return chosenGame;
 };
 
 const haichChuluu = () => {
-  const playAgain = parseInt(prompt("dahin toglohuu"));
-  if (playAgain === 1) {
-    gameChooser();
+  return { winner: 1, result: "user haich com daawuu" };
+};
+
+const coinToss = () => {
+  return { winner: 0, result: "user suld coin too" };
+};
+
+const dice = () => {
+  return { winner: 1, result: "user 6 dice 2" };
+};
+
+const game = () => {
+  let result;
+  const chosenGame = gameChooser();
+  const chosenBalance = parseInt(
+    prompt(`Ta hedeer togloh we? tanii balance: ${balance}`)
+  );
+
+  if (chosenBalance < balance && chosenGame) {
+    result = chosenGame();
+  }
+
+  if (result.winner === 1) {
+    balance = balance + chosenBalance;
+
+    gameResult.push(result);
   } else {
-    result.push({ game: "haichchuluu" });
-    console.log(result);
+    balance = balance - chosenBalance;
+    gameResult.push(result);
+  }
+
+  const playAgain = parseInt(prompt(`Dahiad togloh 1 0`));
+
+  if (playAgain === 1) {
+    game();
+  } else {
+    return console.log(gameResult);
   }
 };
 
-gameChooser();
-result.
+game();
